c8562dd7b4599b41b9c468d46bed875590e02775
lowerants <156000131+lowerants@users.noreply.github.com>
2025-12-02 16:47:10 -0500

    json attempt, workout pages

--- workoutScript.js
+++ workoutScript.js
@@ -1,16 +1,24 @@
-populateTable()
+// populateTable()
 
 // liftDay: String
-function populateTable(liftDay) {
-    const table = document.querySelector("workout_table")
-    // collect JSON data based on the liftDay
+// function populateTable(liftDay) {
+//     const table = document.querySelector("workout_table")
+//     // collect JSON data based on the liftDay
 
-    let numExercises = 2 // will be the number of exercises in the JSON
-    for(let i = 0; i < numExercises; i++) {
-        let tr = document.createElement("tr")
-        // add text of the exercises to the tr
-        // add the tr to the table
-    }
+//     let numExercises = 2 // will be the number of exercises in the JSON
+//     for(let i = 0; i < numExercises; i++) {
+//         let tr = document.createElement("tr")
+//         // add text of the exercises to the tr
+//         // add the tr to the table
+//     }
+// }
+
+tableWorkout()
+function tableWorkout() {
+    let table = document.createElement("table")
+
+    let workoutData = JSON.parse("exercisesList.json")
+    console.log(workoutData)
 }
 
 
