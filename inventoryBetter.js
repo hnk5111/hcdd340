5222f935a5b57c40d23008f9b8e1a5050e84addf
lowerants <156000131+lowerants@users.noreply.github.com>
2025-12-15 22:09:52 -0500

    trying to figure out PWA icons

--- inventoryBetter.js
+++ inventoryBetter.js
@@ -1,65 +1,70 @@
-// JSON because it's easier to save in localStorage and to add stuff into
-let default_inventory = {
-    // belt: {
-    //     name: "lifting belt",
-    //     is_default: true,
-    //     color: "#DDDDDD",
-    //     image_source: ""
-    // },
-    water_bottle: {
-        name: "Water Bottle",
-        is_default: true,
-        color: "#DDDDDD",
-        image_source: "default_inventory_images/water-bottle.png"
-    },
-    shoes: {
-        name: "Powerlifting Shoes",
-        is_default: true,
-        color: "#DDDDDD",
-        image_source: "default_inventory_images/shoes.png"
-    },
-    // straps: {
-    //     name: "wrist straps",
-    //     is_default: true,
-    //     color: "#DDDDDD",
-    //     image_source: ""
-    // },
-    // headphones: {
-    //     name: "headphones",
-    //     is_default: true,
-    //     color: "#DDDDDD",
-    //     image_source: ""
-    // },
-    // snack: {
-    //     name: "snack",
-    //     is_default: true,
-    //     color: "#DDDDDD",
-    //     image_source: ""
-    // }
-};
-
 renderInventory();
 
 // generates the html elements of either default inventory items or the user's
 function renderInventory() {
-    // if(localStorage.getItem("my_inventory") == null) {
+// JSON because it's easier to save in localStorage and to add stuff into
+    let default_inventory = {
+        // belt: {
+        //     name: "lifting belt",
+        //     is_default: true,
+        //     color: "#DDDDDD",
+        //     image_source: ""
+        // },
+        water_bottle: {
+            name: "Water Bottle",
+            is_default: true,
+            color: "#DDDDDD",
+            image_source: "default_inventory_images/water-bottle.png"
+        },
+        shoes: {
+            name: "Powerlifting Shoes",
+            is_default: true,
+            color: "#DDDDDD",
+            image_source: "default_inventory_images/shoes.png"
+        },
+        // straps: {
+        //     name: "wrist straps",
+        //     is_default: true,
+        //     color: "#DDDDDD",
+        //     image_source: ""
+        // },
+        // headphones: {
+        //     name: "headphones",
+        //     is_default: true,
+        //     color: "#DDDDDD",
+        //     image_source: ""
+        // },
+        // snack: {
+        //     name: "snack",
+        //     is_default: true,
+        //     color: "#DDDDDD",
+        //     image_source: ""
+        // }
+    };
+    if(localStorage.getItem("my_inventory") == null) {
         localStorage.setItem("my_inventory", JSON.stringify(default_inventory))
         // localStorage.setItem("my_inventory", default_inventory)
-    // }
+    }
+
+    // parsed version
     let my_inventory = JSON.parse(localStorage.getItem("my_inventory"))
     
     // number of elements in the JSON
     let length = Object.keys(my_inventory).length;
     let keys = Object.keys(my_inventory);
-    for(let j = 0; j < 5; j++) {for(let i = 0; i < length; i++) {
+    for(let i = 0; i < length; i++) {
         let itemJSON = my_inventory[keys[i]]
         createCard(itemJSON)
-    }}
+    }
 }
 
 // actual html element-making
 function createCard(itemJSON) {
     let card = document.createElement("div")
+    // card.id = itemJSON;
+    // if(document.querySelector(card.id) != null) {
+    //     return
+    // }
     
     let image = document.createElement("img")
     image.src = itemJSON.image_source;
@@ -77,6 +82,29 @@ function createCard(itemJSON) {
 }
 
 
+// adds an item to the inventory and the JSON
+// also creates a new card for it
 function addItem() {
-    window.prompt("Question")
+    let newItem = window.prompt("What is the name of your item?")
+
+    let my_inventory = JSON.parse(localStorage.getItem("my_inventory"));
+    let newItemDetails = {
+        name: newItem,
+        is_default: false,
+        color: "#DDDDDD",
+        image_source: "default_inventory_images/dumbell.png"
+    };
+
+    
+    my_inventory.newItem = newItemDetails;
+    localStorage.setItem("my_inventory", JSON.stringify(my_inventory));
+    createCard(my_inventory.newItem);
+
+
+    //     shoes: {
+    //     name: "Powerlifting Shoes",
+    //     is_default: true,
+    //     color: "#DDDDDD",
+    //     image_source: "default_inventory_images/shoes.png"
+    // },
 }
--- testing_camera.js
+++ testing_camera.js
@@ -1,17 +1,34 @@
-const width = 320; // We will scale the photo width to this
-let height = 0; // This will be computed based on the input stream
-
-let streaming = false;
-
-const video = document.getElementById("video");
-const canvas = document.getElementById("canvas");
-const photo = document.getElementById("photo");
-const startButton = document.getElementById("start-button");
-// const allowButton = document.getElementById("permissions-button");
+// <!-- WARNING: DEPRECATED -->
 
 
-// allowButton.addEventListener("click", () => {
-//   window.navigator.mediaDevices
+// const width = 320; // We will scale the photo width to this
+// let height = 0; // This will be computed based on the input stream
+
+// let streaming = false;
+
+// const video = document.getElementById("video");
+// const canvas = document.getElementById("canvas");
+// const photo = document.getElementById("photo");
+// const startButton = document.getElementById("start-button");
+// // const allowButton = document.getElementById("permissions-button");
+
+
+// // allowButton.addEventListener("click", () => {
+// //   window.navigator.mediaDevices
+// //     .getUserMedia({ video: true, audio: false })
+// //     .then((stream) => {
+// //       video.srcObject = stream;
+// //       video.play();
+// //     })
+// //     .catch((err) => {
+// //       console.error(`An error occurred: ${err}`);
+// //     });
+// // });
+
+
+// allowCamera();
+// function allowCamera() {
+//     window.navigator.mediaDevices
 //     .getUserMedia({ video: true, audio: false })
 //     .then((stream) => {
 //       video.srcObject = stream;
@@ -20,73 +37,59 @@ const startButton = document.getElementById("start-button");
 //     .catch((err) => {
 //       console.error(`An error occurred: ${err}`);
 //     });
+// }
+
+
+// video.addEventListener("canplay", (ev) => {
+//   if (!streaming) {
+//     height = video.videoHeight / (video.videoWidth / width);
+
+//     video.setAttribute("width", width);
+//     video.setAttribute("height", height);
+//     canvas.setAttribute("width", width);
+//     canvas.setAttribute("height", height);
+//     streaming = true;
+//   }
 // });
 
 
-allowCamera();
-function allowCamera() {
-    window.navigator.mediaDevices
-    .getUserMedia({ video: true, audio: false })
-    .then((stream) => {
-      video.srcObject = stream;
-      video.play();
-    })
-    .catch((err) => {
-      console.error(`An error occurred: ${err}`);
-    });
-}
+// startButton.addEventListener("click", (ev) => {
+//   takePicture();
+//   ev.preventDefault();
+// });
 
 
-video.addEventListener("canplay", (ev) => {
-  if (!streaming) {
-    height = video.videoHeight / (video.videoWidth / width);
+// function clearPhoto() {
+//   const context = canvas.getContext("2d");
+//   context.fillStyle = "#aaaaaa";
+//   context.fillRect(0, 0, canvas.width, canvas.height);
 
-    video.setAttribute("width", width);
-    video.setAttribute("height", height);
-    canvas.setAttribute("width", width);
-    canvas.setAttribute("height", height);
-    streaming = true;
-  }
-});
+//   const data = canvas.toDataURL("image/png");
+//   // photo.setAttribute("src", data);
+// }
 
+// clearPhoto();
 
-startButton.addEventListener("click", (ev) => {
-  takePicture();
-  ev.preventDefault();
-});
+// function takePicture() {
+//   const context = canvas.getContext("2d");
+//   if (width && height) {
+//     canvas.width = width;
+//     canvas.height = height;
+//     context.drawImage(video, 0, 0, width, height);
 
-
-function clearPhoto() {
-  const context = canvas.getContext("2d");
-  context.fillStyle = "#aaaaaa";
-  context.fillRect(0, 0, canvas.width, canvas.height);
-
-  const data = canvas.toDataURL("image/png");
-  // photo.setAttribute("src", data);
-}
-
-clearPhoto();
-
-function takePicture() {
-  const context = canvas.getContext("2d");
-  if (width && height) {
-    canvas.width = width;
-    canvas.height = height;
-    context.drawImage(video, 0, 0, width, height);
-
-    const data = canvas.toDataURL("image/png");
-    // photo.setAttribute("src", data);
-    // change to default code
-    localStorage.setItem("profilePicture", data)
-  } else {
-    clearPhoto();
-  }
-}
+//     const data = canvas.toDataURL("image/png");
+//     // photo.setAttribute("src", data);
+//     // change to default code
+//     localStorage.setItem("profilePicture", data)
+//   } else {
+//     clearPhoto();
+//   }
+// }
 
 
-if(localStorage.getItem("profilePicture") != null) {
-  let img = document.createElement("img")
-  let div = document.querySelector("#profilePictureDiv")
-  img.src = localStorage.getItem("profilePicture")
-  div.appendChild(img)
-}
+// if(localStorage.getItem("profilePicture") != null) {
+//   let img = document.createElement("img")
+//   let div = document.querySelector("#profilePictureDiv")
+//   img.src = localStorage.getItem("profilePicture")
+//   div.appendChild(img)
+// }
